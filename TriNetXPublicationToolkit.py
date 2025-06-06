b951afc9fa539b981f6a71998726b8ce8235e322
drdanovak <drdanovak@gmail.com>
2025-06-06 00:25:50 -0700

    Create TriNetXPublicationToolkit.py

---
+++ TriNetXPublicationToolkit.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TriNetX Publication Toolkit",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("📚 TriNetX Publication Toolkit")
+st.markdown("""
+Welcome to the **TriNetX Publication Toolkit**, a collection of easy-to-use tools designed to streamline your research publication workflow.
+
+### Tools Available:
+- 🌲 **Forest Plot Generator**
+- 📉 **Kaplan-Meier Survival Curve Viewer**
+- 📊 **Journal-Style Table Generator**
+- 📐 **Effect Size Calculator**
+
+Navigate using the sidebar.
+""")
